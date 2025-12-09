8326b0d420a2258c85bdb2f4b79bcc3edf3e116e
alminsfd <alaminhossaintanvir42@gmail.com>
2025-12-09 16:38:27 +0600

    add  firebase

---
+++ src/Firebase/Firebaseconfig.js
@@ -0,0 +1,11 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCh_ebhpTy_lpHlbkYFukwkBIn9abH6pSg",
+  authDomain: "reporting-system-69.firebaseapp.com",
+  projectId: "reporting-system-69",
+  storageBucket: "reporting-system-69.firebasestorage.app",
+  messagingSenderId: "395850884236",
+  appId: "1:395850884236:web:4a083cc63b06c7dd2d87a9"
+};
+const app = initializeApp(firebaseConfig);
