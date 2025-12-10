032621211738c2c687399aee63a2236941074be7
alminsfd <alaminhossaintanvir42@gmail.com>
2025-12-10 09:55:31 +0600

    loading component add

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -2,7 +2,6 @@
 import { Outlet } from 'react-router';
 import Navbar from '../components/navbar/Navbar';
 import Footer from '../components/footer/Footer';
-
 const MainLayout = () => {
     return (
         <>
