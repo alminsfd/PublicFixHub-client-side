30c4b9dcc0fe76660b359f0c2b4cfc5add9d94f9
alminsfd <alaminhossaintanvir42@gmail.com>
2025-12-10 15:44:36 +0600

    admin protective route add

--- src/routes/AdminRoute.jsx
+++ src/routes/AdminRoute.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
+import useAuth from '../hooks/useAuth';
+import useRole from '../hooks/useRole';
+import Loading from '../components/Loading/Loading';
+import Forbidden from '../components/Forbiddern/Forbidden';
 
-const AdminRoute = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+const AdminRoute = ({ children}) => {
+    const { loading, user } = useAuth();
+    const { role, roleLoading, } = useRole()
+    if (loading || !user || roleLoading) {
+        return <Loading></Loading>
+    }
+    if (role !== 'staff') {
+        return <Forbidden></Forbidden>
+    } 
+    return children
 };
 
 export default AdminRoute;
